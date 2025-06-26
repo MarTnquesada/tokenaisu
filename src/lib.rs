9d64044f0c931f8e3c8cae16d40f6ea9bee59bcf
Martin Quesada <martin@datamaran.com>
2025-06-26 14:06:36 +0900

    subsituted if-else for pattern match

--- src/lib.rs
+++ src/lib.rs
@@ -98,14 +98,13 @@ pub fn moses_tokenize_line(
     let mut tokenized_text = text
         // Remove trailing newline character
         .trim_end_matches('\n')
-        // Replace all sequences of whitespaces with a single whitespace while trimming text
-        // This is done for any type of Unicode space (incl. tabs), so code executed after this is only concerned with strict ASCII spaces
+        // Replace all sequences of whitespaces with a single ASCII whitespace while trimming text
+        // This is done for any type of Unicode space (incl. tabs)
         .split_whitespace()
         .collect::<Vec<&str>>()
         .join(" ");
 
     // Add spaces at the beginning and end of the text
-    tokenized_text.reserve(2);
     tokenized_text.insert(0, ' ');
     tokenized_text.push(' ');
 
@@ -126,37 +125,42 @@ pub fn moses_tokenize_line(
     }
     // After substituting protected patterns, replace all sequences of whitespaces with a single whitespace and trim the text
     tokenized_text = tokenized_text
-        .split_ascii_whitespace()
+        .split_whitespace()
         .collect::<Vec<&str>>()
         .join(" ");
 
     // Separate out all other special characters depending on the language
-    if language == Language::Fi || language == Language::Sv {
-        // In Finnish and Swedish, the colon can be used inside words as an apostrophe-like character:
-        let re_general = Regex::new(r"([^\p{L}\p{N}\s\.\:\'\`\,\-])").unwrap();
-        tokenized_text = re_general.replace_all(&tokenized_text, " $1 ").to_string();
-        // If a colon is not immediately followed by lower-case characters, separate it out anyway
-        let re_colon = Regex::new(r"(:)(?=$|[^\p{Ll}])").unwrap();
-        tokenized_text = re_colon.replace_all(&tokenized_text, " $1 ").to_string();
-    } else if language == Language::Tdt {
-        // # In Tetun, the apostrophe can be used inside words as an apostrophe-like character:
-        let re_general = Regex::new(r"([^\p{L}\p{N}\s\.\'\`\,\-])").unwrap();
-        tokenized_text = re_general.replace_all(&tokenized_text, " $1 ").to_string();
-        // If an apostrophe is not immediately followed by lower-case characters, separate it out anyway
-        let re_apostrophe = Regex::new(r"(')(?=$|[^\p{Ll}])").unwrap();
-        tokenized_text = re_apostrophe
-            .replace_all(&tokenized_text, " $1 ")
-            .to_string();
-    } else if language == Language::Ca {
-        // In Catalan, the middle dot can be used inside words:
-        let re_general = Regex::new(r"([^\p{L}\p{N}\s\.\u{00B7}'\`\,\-])").unwrap();
-        tokenized_text = re_general.replace_all(&tokenized_text, " $1 ").to_string();
-        // If a middot is not immediately followed by lower-case characters, separate it out anywa
-        let re_middot = Regex::new(r"(\u{00B7})(?=$|[^\p{Ll}])").unwrap();
-        tokenized_text = re_middot.replace_all(&tokenized_text, " $1 ").to_string();
-    } else {
-        let re_general = Regex::new(r"([^\p{L}\p{N}\s\.\'\`\,\-])").unwrap();
-        tokenized_text = re_general.replace_all(&tokenized_text, " $1 ").to_string();
+    match language {
+        Language::Fi | Language::Sv => {
+            // In Finnish and Swedish, the colon can be used inside words as an apostrophe-like character:
+            let re_general = Regex::new(r"([^\p{L}\p{N}\s\.\:\'\`\,\-])").unwrap();
+            tokenized_text = re_general.replace_all(&tokenized_text, " $1 ").to_string();
+            // If a colon is not immediately followed by lower-case characters, separate it out anyway
+            let re_colon = Regex::new(r"(:)(?=$|[^\p{Ll}])").unwrap();
+            tokenized_text = re_colon.replace_all(&tokenized_text, " $1 ").to_string();
+        }
+        Language::Tdt => {
+            // # In Tetun, the apostrophe can be used inside words as an apostrophe-like character:
+            let re_general = Regex::new(r"([^\p{L}\p{N}\s\.\'\`\,\-])").unwrap();
+            tokenized_text = re_general.replace_all(&tokenized_text, " $1 ").to_string();
+            // If an apostrophe is not immediately followed by lower-case characters, separate it out anyway
+            let re_apostrophe = Regex::new(r"(')(?=$|[^\p{Ll}])").unwrap();
+            tokenized_text = re_apostrophe
+                .replace_all(&tokenized_text, " $1 ")
+                .to_string();
+        }
+        Language::Ca => {
+            // In Catalan, the middle dot can be used inside words:
+            let re_general = Regex::new(r"([^\p{L}\p{N}\s\.\u{00B7}'\`\,\-])").unwrap();
+            tokenized_text = re_general.replace_all(&tokenized_text, " $1 ").to_string();
+            // If a middot is not immediately followed by lower-case characters, separate it out anywa
+            let re_middot = Regex::new(r"(\u{00B7})(?=$|[^\p{Ll}])").unwrap();
+            tokenized_text = re_middot.replace_all(&tokenized_text, " $1 ").to_string();
+        }
+        _ => {
+            let re_general = Regex::new(r"([^\p{L}\p{N}\s\.\'\`\,\-])").unwrap();
+            tokenized_text = re_general.replace_all(&tokenized_text, " $1 ").to_string();
+        }
     }
 
     // Optional aggressive hyphen splitting
@@ -286,7 +290,7 @@ pub fn moses_tokenize_line(
 
     // Word tokenization
     let words: Vec<&str> = tokenized_text.split_whitespace().collect();
-    let mut word_tokenized_text = String::new();
+    let mut word_tokenized_text: String = String::new();
     let re_period = Regex::new(r"^(\S+)\.$").unwrap();
     for (i, word) in words.iter().enumerate() {
         let mut processed_word = word.to_string();
@@ -425,18 +429,19 @@ mod tests {
         assert_eq!(result, "Ich hoffe , daß Sie schöne Ferien hatten .\n");
     }
 
-    #[test]
-    fn chinese_simple() {
-        let result =
-            moses_tokenize_line("这是一个简单的的汉语句子。", Language::En, true, false, &[]);
-        assert_eq!(result, "这 是 一个 简单 的的 汉语 句子 。\n");
-    }
+    // TODO Japanese/Korean/Chinese CJK characters are handle by Moses detokenizer (https://github.com/moses-smt/mosesdecoder/blob/master/scripts/tokenizer/detokenizer.perl), but not by the tokenizer
+    // #[test]
+    // fn chinese_simple() {
+    //     let result =
+    //         moses_tokenize_line("这是一个简单的的汉语句子。", Language::En, true, false, &[]);
+    //     assert_eq!(result, "这 是 一个 简单 的的 汉语 句子 。\n");
+    // }
 
-    #[test]
-    fn japanese_simple() {
-        let result = moses_tokenize_line("どうしょうかな。", Language::En, true, false, &[]);
-        assert_eq!(result, "どう しょ う か な 。\n");
-    }
+    // #[test]
+    // fn japanese_simple() {
+    //     let result = moses_tokenize_line("どうしょうかな。", Language::En, true, false, &[]);
+    //     assert_eq!(result, "どう しょ う か な 。\n");
+    // }
 
     #[test]
     fn protected_patterns() {
